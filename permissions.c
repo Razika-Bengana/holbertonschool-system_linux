19a72b17b78c000b1ade971d695e97d2068145e6
RazikaBengana <razikabengana@gmail.com>
2023-10-05 00:40:00 +0200

    Add permissions for the -l option

---
+++ permissions.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void permissions(struct stat file_stat)
+{
+    printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
+    printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
+    printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
+}
