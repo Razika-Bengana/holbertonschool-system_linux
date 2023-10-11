7b7ecc009bbb93cb4474d78b9f5b8acffc82ac4b
Razika-Bengana <5003@holbertonstudents.com>
2023-10-11 17:43:36 +0200

    rearrange some points

---
+++ ls/my_strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int my_strcmp(const char *str1, const char *str2)
+{
+    while (*str1 && *str2)
+    {
+        if (*str1 != *str2)
+        {
+            return (unsigned char)(*str1) - (unsigned char)(*str2);
+        }
+        str1++;
+        str2++;
+    }
+    return (unsigned char)(*str1) - (unsigned char)(*str2);
+}
