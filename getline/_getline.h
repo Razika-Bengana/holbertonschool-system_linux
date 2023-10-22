08c4d8a10c5d0299b54a39fbee36016ce6cf49c0
RazikaBengana <razikabengana@gmail.com>
2023-10-23 00:15:38 +0200

    Function that reads an entire line from a file descriptor

--- getline/_getline.h
+++ getline/_getline.h
@@ -1,8 +1,14 @@
-#ifndef _GETLINE_H_
-#define _GETLINE_H_
+#ifndef _GETLINE_H
+#define _GETLINE_H
 
-#define READ_SIZE 1024
+#define READ_SIZE 4096
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *buffer_append(char *buffer, char *append, size_t len);
 char *_getline(const int fd);
 
-#endif /* _GETLINE_H_ */
+
+#endif /* _GETLINE_H */
