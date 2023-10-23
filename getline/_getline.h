4224465b03565f57f29a2e03f44d097154a69d15
RazikaBengana <razikabengana@gmail.com>
2023-10-23 16:29:04 +0200

    Update header file

--- getline/_getline.h
+++ getline/_getline.h
@@ -1,13 +1,11 @@
 #ifndef _GETLINE_H
 #define _GETLINE_H
 
-#define READ_SIZE 4096
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-char *buffer_append(char *buffer, char *append, size_t len);
 char *_getline(const int fd);
 
 
