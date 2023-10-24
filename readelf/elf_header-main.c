619456a37b5ad15140b40e5de40a205414f7b79e
RazikaBengana <razikabengana@gmail.com>
2023-10-24 21:11:05 +0200

    Refactor the code to respect betty style add a description for each function

--- readelf/elf_header-main.c
+++ readelf/elf_header-main.c
@@ -1,25 +1,87 @@
 #include "elf_header.h"
 
 /**
+ * initialize_elf_header - program that initializes and read the ELF header from a file
+ *
+ * this function reads the first few bytes of the file to determine
+ * whether it's an ELF32 or ELF64 file;
+ * it then dynamically allocates memory for the ELF header and reads it
+ * into this memory
+ *
+ * @file: a pointer to the file to read from
+ * @is_elf64_ptr: a pointer to an integer where the determination of the ELF class (32/64-bit) will be stored
+ *
+ * Return: a pointer to the dynamically allocated ELF header, or NULL if the operation fails
+ */
+
+void* initialize_elf_header(FILE *file, int *is_elf64_ptr)
+{
+    unsigned char e_ident[EI_NIDENT];
+    if (fread(e_ident, EI_NIDENT, 1, file) != 1)
+    {
+        perror("Error reading e_ident");
+        return (NULL);
+    }
+
+    *is_elf64_ptr = (e_ident[EI_CLASS] == ELFCLASS64);
+    void *header = *is_elf64_ptr ? (void *)malloc(sizeof(Elf64_Ehdr)) : (void *)malloc(sizeof(Elf32_Ehdr));
+
+    if (header == NULL)
+    {
+        perror("Error allocating memory for header");
+    }
+
+    fseek(file, 0, SEEK_SET);
+    read_elf_header(file, header, *is_elf64_ptr);
+    return (header);
+}
+
+/**
+ * print_elf_header_details - program that prints the details of the ELF header
+ *
+ * this function takes a pointer to an ELF header and prints its details,
+ * such as the magic number, class, data encoding, etc...;
+ * it also determines whether the header is for an ELF32 or ELF64 file
+ * and prints the details accordingly
+ *
+ * @header: a pointer to the ELF header to print
+ * @is_elf64: an integer flag that indicates whether the file is ELF32 or ELF64
+ *
+ * Return: nothing (void)
+ */
+
+void print_elf_header_details(void *header, int is_elf64)
+{
+    printf("ELF Header:\n");
+    print_magic(header, is_elf64);
+    print_class(header, is_elf64);
+    print_data(header, is_elf64);
+    print_version(header, is_elf64);
+    print_os_abi(header, is_elf64);
+    print_abi_version(header, is_elf64);
+    print_type(header, is_elf64);
+    print_machine_type(header, is_elf64);
+    print_header_details(header, is_elf64);
+}
+
+/**
  * main - the entry point
  *
  * program that reads and displays the ELF header of an ELF file
  *
- * this function takes an ELF file as an argument and reads its ELF header
- * to display various fields such as magic number, class, data encoding,
- * version, OS ABI, ABI version, file type, machine type, etc...;
+ * this function takes the path of an ELF file as a command-line argument;
+ * it opens the file, reads the ELF header, and then calls a series of functions
+ * to print the details of this header;
  * it supports both ELF32 and ELF64 file formats;
  *
- * @argc: the number of command-line arguments
- * @argv: the command-line arguments --> argv[1] should be the ELF file path
+ * @argc: number of command-line arguments.
+ * @argv: command-line arguments --> argv[1] should contain the ELF file path
  *
  * Return: 0 on success, 1 on failure
  */
 
 int main(int argc, char *argv[])
 {
-    void *header = NULL;
-
     if (argc != 2)
     {
         printf("Usage: %s <ELF_FILE>\n", argv[0]);
@@ -33,38 +95,16 @@ int main(int argc, char *argv[])
         return (1);
     }
 
-    unsigned char e_ident[EI_NIDENT];
-    if (fread(e_ident, EI_NIDENT, 1, file) != 1)
-    {
-        perror("Error reading e_ident");
-        fclose(file);
-        return 1;
-    }
-
-    int is_elf64 = (e_ident[EI_CLASS] == ELFCLASS64);
-    header = is_elf64 ? (void *)malloc(sizeof(Elf64_Ehdr)) : (void *)malloc(sizeof(Elf32_Ehdr));
+    int is_elf64;
+    void *header = initialize_elf_header(file, &is_elf64);
 
     if (header == NULL)
     {
-        perror("Error allocating memory for header");
         fclose(file);
-        return 1;
+        return (1);
     }
 
-    fseek(file, 0, SEEK_SET);
-
-    read_elf_header(file, header, is_elf64);
-
-    printf("ELF Header:\n");
-    print_magic(header, is_elf64);
-    print_class(header, is_elf64);
-    print_data(header, is_elf64);
-    print_version(header, is_elf64);
-    print_os_abi(header, is_elf64);
-    print_abi_version(header, is_elf64);
-    print_type(header, is_elf64);
-    print_machine_type(header, is_elf64);
-    print_header_details(header, is_elf64);
+    print_elf_header_details(header, is_elf64);
 
     free(header);
     fclose(file);
--- readelf/elf_header.h
+++ readelf/elf_header.h
@@ -11,20 +11,31 @@
 
 
 /* task 0: ELF file header */
+
+void* initialize_elf_header(FILE *file, int *is_elf64_ptr);
+void print_elf_header_details(void *header, int is_elf64);
+
 void swap_bytes(void *value, size_t size);
 int system_is_little_endian(void);
 int system_is_big_endian(void);
 
+void print_header_details_64(Elf64_Ehdr *header);
+void print_header_details_32(Elf32_Ehdr *header);
+void print_header_details(void *header, int is_elf64);
+
+void swap_fields_64(Elf64_Ehdr *hdr64);
+void swap_fields_32(Elf32_Ehdr *hdr32);
+void print_data_encoding(unsigned char elf_data);
+void print_data(void *header, int is_elf64);
+
 void read_elf_header(FILE *file, void *header, int is_elf64);
 void print_magic(void *header, int is_elf64);
 void print_class(void *header, int is_elf64);
-void print_data(void *header, int is_elf64);
 void print_version(void *header, int is_elf64);
 void print_os_abi(void *header, int is_elf64);
 void print_abi_version(void *header, int is_elf64);
 void print_type(void *header, int is_elf64);
 void print_machine_type(void *header, int is_elf64);
-void print_header_details(void *header, int is_elf64);
 
 /* task 1: ELF sections' headers */
 
