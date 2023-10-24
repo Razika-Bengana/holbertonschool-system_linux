cb31f021924c8f337c8fbb2e2c8d8a9addea73c5
RazikaBengana <razikabengana@gmail.com>
2023-10-24 13:25:30 +0200

    Display the "unknown" case of os_abi in hexadecimal

--- readelf/elf_header.c
+++ readelf/elf_header.c
@@ -97,7 +97,7 @@ void print_os_abi(void *header, int is_elf64)
             printf("UNIX - FreeBSD\n");
             break;
         default:
-            printf("<unknown: %d>\n", elf_osabi);
+            printf("<unknown: %x>\n", elf_osabi);
             break;
     }
 }
