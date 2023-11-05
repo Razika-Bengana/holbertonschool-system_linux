1ec6ff2d5f0794d9e652c4a3c88d40957a775316
RazikaBengana <razikabengana@gmail.com>
2023-11-05 13:31:49 +0100

    Adjust symbol types and binding display in ELF symbol table output. This update determines if a symbol is visible and can be linked from other files or if its use is restricted to the file where it is defined

--- nm_objdump/test2.c
+++ nm_objdump/test2.c
@@ -1,70 +1,58 @@
 #include "main.h"
 
-void print_symbol_table64(Elf64_Shdr *section_header, Elf64_Sym *symbol_table, char *string_table)
+void print_symbol_table64(Elf64_Shdr *section_header, Elf64_Sym *symbol_table, char *string_table, Elf64_Shdr *section_headers)
 {
-    int symbol_count = section_header->sh_size / sizeof(Elf64_Sym);
     int i;
+    int symbol_count = section_header->sh_size / sizeof(Elf64_Sym);
+
     for (i = 0; i < symbol_count; i++)
     {
         Elf64_Sym symbol = symbol_table[i];
         char *symbol_name = string_table + symbol.st_name;
+
+        /* On s'assure que le nom du symbole n'est pas nul et en plus qu'il ne s'agit pas d'un fichier */
         if (symbol.st_name != 0 && ELF64_ST_TYPE(symbol.st_info) != STT_FILE)
         {
-            char symbol_type = '?';
-            switch (ELF64_ST_TYPE(symbol.st_info))
-            {
-                case STT_NOTYPE:
-                    if (ELF64_ST_BIND(symbol.st_info) == STB_GLOBAL || ELF64_ST_BIND(symbol.st_info) == STB_WEAK)
-                    {
-                        symbol_type = 'w';
-                    }
-                    else
-                    {
-                        symbol_type = 'n';
-                    }
-                    break;
-                case STT_OBJECT:
-                    symbol_type = (ELF64_ST_BIND(symbol.st_info) == STB_GLOBAL) ? 'D' : 'd';
-                    break;
-                case STT_FUNC:
-                    symbol_type = (ELF64_ST_BIND(symbol.st_info) == STB_GLOBAL) ? 'T' : 't';
-                    break;
-                case STT_SECTION:
-                    symbol_type = 's';
-                    break;
-                case STT_FILE:
-                    symbol_type = 'f';
-                    break;
-                case STT_TLS:
-                    symbol_type = (ELF64_ST_BIND(symbol.st_info) == STB_GLOBAL) ? 'D' : 'd';
-                    break;
-                case STT_LOOS:
-                    symbol_type = 'B';
-                    break;
-                case STT_HIOS:
-                    symbol_type = 'b';
-                    break;
-                case STT_LOPROC:
-                    symbol_type = 'R';
-                    break;
-                case STT_HIPROC:
-                    symbol_type = 'r';
-                    break;
-            }
-            if (symbol.st_shndx == SHN_UNDEF)
-            {
+            char symbol_type = '?';  /* Type de symbole inconnu */
+            Elf64_Shdr symbol_section = section_headers[symbol.st_shndx];
+
+            /* Vérifier d'abord les symboles faibles et uniques */
+            if (ELF64_ST_BIND(symbol.st_info) == STB_GNU_UNIQUE)
+                symbol_type = 'u';
+            else if (ELF64_ST_BIND(symbol.st_info) == STB_WEAK && ELF64_ST_TYPE(symbol.st_info) == STT_OBJECT)
+                symbol_type = symbol.st_shndx == SHN_UNDEF ? 'v' : 'V';
+            else if (ELF64_ST_BIND(symbol.st_info) == STB_WEAK)
+                symbol_type = symbol.st_shndx == SHN_UNDEF ? 'w' : 'W';
+
+            /* Ensuite, vérifier les indices de section spéciaux */
+            else if (symbol.st_shndx == SHN_UNDEF)
                 symbol_type = 'U';
-            }
-            /* weak symbols */
-            if (ELF64_ST_BIND(symbol.st_info) == STB_WEAK)
+            else if (symbol.st_shndx == SHN_ABS)
+                symbol_type = 'A';
+            else if (symbol.st_shndx == SHN_COMMON)
+                symbol_type = 'C';
+
+            /* Vérifier les types de section et les flags */
+            else if (symbol_section.sh_type == SHT_NOBITS && symbol_section.sh_flags == (SHF_ALLOC | SHF_WRITE))
+                symbol_type = 'B';
+            else if (symbol_section.sh_type == SHT_PROGBITS && symbol_section.sh_flags == SHF_ALLOC)
+                symbol_type = 'R';
+            else if (symbol_section.sh_type == SHT_PROGBITS && symbol_section.sh_flags == (SHF_ALLOC | SHF_WRITE))
+                symbol_type = 'D';
+            else if (symbol_section.sh_type == SHT_PROGBITS && symbol_section.sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
+                symbol_type = 'T';
+            else if (symbol_section.sh_type == SHT_DYNAMIC)
+                symbol_type = 'D';
+            /* convertir en minuscule si le symbole est local */
+            if (ELF64_ST_BIND(symbol.st_info) == STB_LOCAL)
             {
-                symbol_type = (symbol_type == 'D' || symbol_type == 'd') ? 'W' : 'w';
+                symbol_type = tolower(symbol_type);
             }
+
             printf("%016lx %c %s\n", symbol.st_value, symbol_type, symbol_name);
         }
     }
 }
-
 
 
 void process_elf_file64(char *file_path)
@@ -148,7 +136,9 @@ void process_elf_file64(char *file_path)
 
     /* printer la table des symboles */
     printf("Adresse       Type Symbole\n");
-    print_symbol_table64(&symbol_table_header, symbol_table, string_table);
+    // Supposons que section_headers est votre tableau d'en-têtes de section.
+    print_symbol_table64(&symbol_table_header, symbol_table, string_table, section_headers);
+
 
     fclose(file);
     free(section_headers);
