c384038e54854f1dc8191f086b1a86fdcd20b62c
RazikaBengana <razikabengana@gmail.com>
2023-11-12 13:02:06 +0100

    Remove an extra space after the ASCII side

--- nm_objdump/hobjdump-section_content.c
+++ nm_objdump/hobjdump-section_content.c
@@ -89,7 +89,7 @@ void print_section_content(FILE *file, void *shdr_generic, int is_64, const char
 
             if (i % 16 != 15)
             {
-                int padding_for_alignment = (16 - (i % 16));
+                int padding_for_alignment = (16 - (i % 16)) - 1;
                 printf("%*s", padding_for_alignment, "");
             }
 
