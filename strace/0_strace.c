d1d74795fcd300ffdaf420190130e7c953f69da1
RazikaBengana <razikabengana@gmail.com>
2023-12-17 22:35:59 +0100

    0. Step #0 - Syscall number

--- strace/0_strace.c
+++ strace/0_strace.c
@@ -1,74 +1,82 @@
 #include "strace.h"
 
 /**
- * execute_trace - program that combines tracing of child and parent process
+ * execute_and_trace_process - program that executes and traces a child process
  *
- * this function handles both child and parent process tracing;
- * for the child, it sets up the tracing and executes the given command;
- * for the parent, it handles the syscall tracing and output
+ * this function is responsible for setting up tracing for a child process
+ * and executing a given command;
+ * it also handles errors during execution
  *
- * @args: argument vector for execve
- * @env: environ for execve
- * @child_pid: pid of child, 0 if current process is child
+ * @command_args: argument vector for execve
+ * @environment: environment for execve
  *
  * Return: nothing (void)
  */
 
-void execute_trace(char **args, char **env, pid_t child_pid)
+void execute_and_trace_process(char **command_args, char **environment)
 {
-	int status;
-	struct user_regs_struct regs;
-	int in_syscall = 0;
+	ptrace(PTRACE_TRACEME, 0, 0, 0);
+	kill(getpid(), SIGSTOP);
 
-	if (child_pid == 0)
-	{  /* Child process */
-		ptrace(PTRACE_TRACEME);
-		kill(getpid(), SIGSTOP);
-
-		if (execve(args[1], args + 1, env) == -1)
-		{
-			fprintf(stderr, "Exec failed: %d\n", errno);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	else
-	{  /* Parent process */
-		waitpid(child_pid, &status, 0);
-		ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
-
-		while (1)
-		{
-			ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-			waitpid(child_pid, &status, 0);
-
-			if (WIFEXITED(status)) break;
-
-			if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
-			{
-				if (in_syscall)
-				{
-					memset(&regs, 0, sizeof(regs));
-					ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-					printf("%lu\n", (unsigned long)regs.orig_rax);
-				}
-				in_syscall = !in_syscall;
-			}
-		}
+	if (execve(command_args[1], command_args + 1, environment) == -1)
+	{
+		fprintf(stderr, "Exec failed: %d\n", errno);
+		exit(EXIT_FAILURE);
 	}
 }
 
 
 
 /**
+ * monitor_and_trace_syscalls - program that monitors and traces system calls of a child process
+ *
+ * this function handles tracing of system calls made by the child process;
+ * it waits for system call events from the child and prints them
+ *
+ * @child_pid: PID of the child process to monitor
+ *
+ * Return: nothing (void)
+ */
+
+void monitor_and_trace_syscalls(pid_t child_pid)
+{
+	int status;
+	struct user_regs_struct regs;
+	int is_syscall_entry = 1;
+
+	waitpid(child_pid, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
+
+	while (1)
+	{
+		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
+		waitpid(child_pid, &status, 0);
+
+		if (WIFEXITED(status))
+			break;
+
+		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+		{
+			if (is_syscall_entry)
+			{
+				memset(&regs, 0, sizeof(regs));
+				ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+				printf("%lu\n", (unsigned long)regs.orig_rax);
+			}
+			is_syscall_entry = !is_syscall_entry;
+		}
+	}
+}
+
+/**
  * main - the entry point
  *
  * this function is the starting point of the program;
- * it initializes the process, decides if the current process is the parent or the child for tracing,
- * and then calls the "execute_trace" function to perform the actual tracing
+ * it forks the process into a parent and child;
+ * the child process executes the command, and the parent traces the child's system calls
  *
- * @argc: the argument count
- * @argv: the argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * @envp: environment variables
  *
  * Return: EXIT_SUCCESS on successful execution, otherwise EXIT_FAILURE
@@ -84,17 +92,20 @@ int main(int argc, char **argv, char **envp)
 		return (EXIT_FAILURE);
 	}
 
-	setvbuf(stdout, NULL, _IONBF, 0);
-
 	child_pid = fork();
-
 	if (child_pid == -1)
 	{
 		fprintf(stderr, "Fork failed: %d\n", errno);
 		exit(EXIT_FAILURE);
 	}
-
-	execute_trace(argv, envp, child_pid);
+	else if (child_pid == 0)
+	{
+		execute_and_trace_process(argv, envp);
+	}
+	else
+	{
+		monitor_and_trace_syscalls(child_pid);
+	}
 
 	return (EXIT_SUCCESS);
 }
