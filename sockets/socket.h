92af05e7b11459c63a88a0718e119bdae4dd2063
RazikaBengana <razikabengana@gmail.com>
2024-02-09 09:42:30 +0100

    Header file

---
+++ sockets/socket.h
@@ -0,0 +1,11 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#endif /* SOCKET_H */
