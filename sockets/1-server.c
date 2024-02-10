a5081b09cd7c545ef79e53125b656972a614f794
RazikaBengana <razikabengana@gmail.com>
2024-02-10 19:58:04 +0100

    1. Accept

---
+++ sockets/1-server.c
@@ -0,0 +1,69 @@
+#include "socket.h"
+
+/**
+ * main - the entry point
+ *
+ * this program creates an IPv4 TCP server listening on port 12345;
+ * it initializes a server socket and binds it to the specified port
+ * to listen for incoming connections;
+ * upon a successful connection, it retrieves and prints the IP address
+ * of the connected client;
+ * the server then closes the client socket and terminates;
+ * this server demonstrates basic socket programming concepts such as socket creation,
+ * binding, listening for connections, and accepting connections
+ *
+ * Return: 0 on successful execution,
+ * 	   or exits with a failure status if any socket operation fails
+ */
+
+int main(void)
+{
+	int server_fd, new_socket;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(12345);
+
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 3) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Server listening on port 12345\n");
+
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	char client_ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
+	printf("Client connected: %s\n", client_ip);
+
+	close(new_socket);
+	close(server_fd);
+
+	return (0);
+}
