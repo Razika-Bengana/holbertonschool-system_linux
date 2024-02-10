fb6795641dd7bbf65ef403d0da57f906249ded11
RazikaBengana <razikabengana@gmail.com>
2024-02-10 21:51:26 +0100

    Update the header file

--- sockets/socket.h
+++ sockets/socket.h
@@ -9,4 +9,17 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+/* task 0 */
+int create_server_socket(void);
+void configure_server_socket(int server_fd);
+void initialize_address(struct sockaddr_in *address);
+void bind_server_socket(int server_fd, struct sockaddr_in *address);
+void start_listening(int server_fd);
+
+
+/* task 1 */
+void initialize_address(struct sockaddr_in *address);
+int create_and_setup_server_socket(struct sockaddr_in *address);
+
+
 #endif /* SOCKET_H */
