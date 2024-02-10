b9169f73a14f91e7958ac4978c4f7f29311a2d68
RazikaBengana <razikabengana@gmail.com>
2024-02-10 20:55:11 +0100

    Remove extra lines

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -17,17 +17,19 @@
  *         or exits with a failure status if any socket operation fails
  */
 
-int main(void) {
+int main(void)
+{
 	struct sockaddr_in address = {0};
-	int server_fd, new_socket, opt = 1;
-	int addrlen = sizeof(address);
+	int server_fd, new_socket, opt = 1, addrlen = sizeof(address);
 	char client_ip[INET_ADDRSTRLEN];
 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
@@ -35,7 +37,8 @@ int main(void) {
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(12345);
 
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
@@ -44,8 +47,8 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 	printf("Server listening on port 12345\n");
-
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+	{
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
